695674c319cff1338ecb5e3be216a2fa8e1d1bd6
Dhruv Kaushik <dhruvkaushikjee9457@gmail.com>
2026-09-25 00:40:20 +0530

    add T05DaemonThread.java

---
+++ rebuilds/threads-concurrency-25-steps/T05DaemonThread.java
@@ -0,0 +1,22 @@
+public class T05DaemonThread{
+    public static void main(String[] args) throws InterruptedException{
+        Thread heartbeat = new Thread(() ->{
+            while(true){
+                try{Thread.sleep(50);} catch(InterruptedException e){return;}
+            }
+        }, "heartbeat");
+        heartbeat.setDaemon(true);
+        heartbeat.start();
+        System.out.println("daemon " + heartbeat.isDaemon());
+        System.out.println("inherits " + new Thread(() -> {}).isDaemon());
+        Thread worker = new Thread(() -> System.out.println("worker"), "worker");
+        worker.start();
+        worker.join();
+        try{
+            heartbeat.setDaemon(false);
+        }catch(IllegalThreadStateException e){
+            System.out.println("too late " + e.getClass().getSimpleName());
+        }
+        System.out.println("exit");
+    }
+}
